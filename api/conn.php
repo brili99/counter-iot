8277c0e2132025980861a5ca46e593df1c29011c
Muhammad Brilian Erranaomi <49330514+brili99@users.noreply.github.com>
2021-11-04 12:40:12 +0700

    Perubahan 2021-11-04
    Perubahan di index.html pada tabel, memperbanyak data menjadi 6 status.
    Penambahan paket installer dengan npm untuk offline server

---
+++ api/conn.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "counter";
+$password = "bismillah";
+$dbname = "tppri_counter";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//cek tabel ada atau tidak
+$sql = "SHOW TABLES LIKE 'status'";
+$result = $conn->query($sql);
+if ($result->num_rows == 0) {
+    //jika tidak ada tabel status, maka dibuat dan isi
+    $sql = "CREATE TABLE status (btn INT PRIMARY KEY, counter INT NOT NULL, last_update TIMESTAMP);";
+    $sql .= "INSERT INTO status (btn, counter, last_update) VALUES ('1','0',now()),('2','0',now()),('3','0',now()),('4','0',now()),('5','0',now()),('6','0',now());";
+    if ($conn->multi_query($sql) === FALSE) {
+        echo "Error updating record: " . $conn->error;
+    }
+}
---
+++ server.py
@@ -0,0 +1,93 @@
+import RPi.GPIO as GPIO
+import mysql.connector
+from time import sleep
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="counter",
+    password="bismillah",
+    database="tppri_counter"
+)
+
+pin_btn1 = 11
+pin_btn2 = 5
+pin_btn3 = 6
+pin_btn4 = 13
+pin_btn5 = 19
+pin_btn6 = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_btn1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_btn2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_btn3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_btn4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_btn5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_btn6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+btn1 = GPIO.input(pin_btn1)
+btn2 = GPIO.input(pin_btn2)
+btn3 = GPIO.input(pin_btn3)
+btn4 = GPIO.input(pin_btn4)
+btn5 = GPIO.input(pin_btn5)
+btn6 = GPIO.input(pin_btn6)
+
+
+def add_counter(nmr_btn):
+    mycursor = mydb.cursor(buffered=True , dictionary=True)
+    mycursor.execute("SHOW TABLES LIKE 'status'")
+    if mycursor.rowcount == 0:
+        print("tables status is missing, creating table now")
+        import os
+        os.system('php api/conn.php')
+        sleep(0.5)
+        # return
+    print("adding counter")
+    mycursor.execute("SELECT counter FROM status WHERE btn='"+str(nmr_btn)+"'")
+    myresult = mycursor.fetchall()
+    # print(myresult[0]["counter"])
+    counter = int(myresult[0]["counter"]) + 1
+    mycursor.execute("UPDATE status SET counter='" +
+                     str(counter)+"' WHERE btn='"+str(nmr_btn)+"'")
+    mydb.commit()
+    if mycursor.rowcount > 0:
+        print("success adding counter btn "+str(nmr_btn))
+    else:
+        print("fail addding counter btn "+str(nmr_btn))
+    mycursor.close()
+    
+def do_loop():
+    print("server is ready.")
+    while True:
+        # print("waiting")
+        if GPIO.input(pin_btn1) == 0:
+            # print("triggered")
+            while GPIO.input(pin_btn1) != 1:
+                sleep(0.01)
+            add_counter(int(1))
+        if GPIO.input(pin_btn2) == 0:
+            while GPIO.input(pin_btn2) != 1:
+                sleep(0.01)
+            add_counter(int(2))
+        if GPIO.input(pin_btn3) == 0:
+            while GPIO.input(pin_btn3) != 1:
+                sleep(0.01)
+            add_counter(int(3))
+        if GPIO.input(pin_btn4) == 0:
+            while GPIO.input(pin_btn4) != 1:
+                sleep(0.01)
+            add_counter(int(4))
+        if GPIO.input(pin_btn5) == 0:
+            while GPIO.input(pin_btn5) != 1:
+                sleep(0.01)
+            add_counter(int(5))
+        if GPIO.input(pin_btn6) == 0:
+            while GPIO.input(pin_btn6) != 1:
+                sleep(0.01)
+            add_counter(int(6))
+        sleep(0.1)
+    
+# do_loop()
+try:
+    do_loop()
+except:
+    print("An exception occurred")
